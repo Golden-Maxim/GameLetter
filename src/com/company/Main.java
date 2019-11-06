b42e40e16b6b48e7a6365ab120d7ff5bddd10397
Maksim <fkca.2ci14.cmm@gmail.com>
2019-11-06 22:33:29 +0200

    Added methods which can be tested by JUnit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,66 +3,93 @@ import java.util.Random;
 import java.util.Scanner;
 import java.lang.*;
 
-
+// just for see alphabet
+//{"A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "X", "Y", "Z"}
 
 public class Main {
 
+    public static char ComputerLetter(){
+        Random rnd = new Random();
+        return (char)(rnd.nextInt((90 - 65) + 1) + 65);
+    }
+
+    public static boolean CheckLengthString(String inputString){
+        if(inputString.length() > 1) {
+            System.out.println("Error! String has more than one letter!");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean CheckLatinLetter(char letter){
+        return (int)letter < 65 || (int)letter > 90;
+    }
+
+    public static boolean CompareLetter(int a , int b)
+    {
+        if(a == b){
+            System.out.println("YOU WON!!!");
+            return true;
+        }
+        else
+        {
+            System.out.println("Try again!");
+            return false;
+        }
+    }
+
+    public static boolean LowHightLetter(int a , int b)
+    {
+        if(a > b){
+            System.out.println("Too high!");
+            return true;
+        }
+        else
+        {
+            System.out.println("Too low!");
+            return false;
+        }
+    }
+
+
     public static void main(String[] args) {
 
-        //choose a random element
-        Random rnd = new Random();
-        char randomElement = (char)(rnd.nextInt((90 - 65) + 1) + 65);
-
-        //String alphabet[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "X", "Y", "Z"};
-        //char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z'};
         int triesCount = 0;
         char userLetter;
         String inputString = "";
 
+        char compLetter = ComputerLetter();
+
         System.out.println("Game Guess the letter");
-
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the letter: ");
 
+        Scanner scanner = new Scanner(System.in);
 
-     do {
-         triesCount++;
+        do {
+            triesCount++;
 
-         inputString = scanner.next();
-         userLetter = inputString.charAt(0);
+            inputString = scanner.next();
+            userLetter = inputString.charAt(0);
 
-         // check a length of string
-         if(inputString.length() > 1) {
-             System.out.println("Error!");
-             continue;
-         }
-         userLetter = Character.toUpperCase(userLetter);
+            if(!CheckLengthString(inputString)){
+                continue;
+            }
 
-         if ((int)userLetter < 65 || (int)userLetter > 90)
-         {
-             System.out.println("Please to get acquainted with the rule of the game, it's not a symbol of Latin alphabet");
-             System.out.println("Enter one more times");
-            continue;
+            userLetter = Character.toUpperCase(userLetter);
 
-         }
+            if(CheckLatinLetter(userLetter)){
+                System.out.println("Please to get acquainted with the rule of the game, it's not a symbol of Latin alphabet");
+                System.out.println("Enter one more times");
+                continue;
+            }
 
+            CompareLetter((int)userLetter,(int)compLetter);
+            LowHightLetter((int)userLetter,(int)compLetter);
 
-         if (userLetter == randomElement)
-         {
-             System.out.println("YOU WON!!!");
-         } else if ((int)userLetter > (int)randomElement) {
-             System.out.println("Too high!");
-         } else if ((int)userLetter < (int)randomElement) {
-             System.out.println("Too low!");
-         }
-
-
-     } while (userLetter != randomElement);
+        } while (userLetter != compLetter);
 
         System.out.println("Count of trys:" + triesCount);
 
-
-}
-
+    }
 }
